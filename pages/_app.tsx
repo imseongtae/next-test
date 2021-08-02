8e649cfe09298e4b1de8fdc1366d779f29f57541
imseongtae <dev.haemil@gmail.com>
2021-08-03 03:03:07 +0900

    feat: Update _app to include globals scss and next/head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head';
 import '../styles/common/reset.css';
 import '../styles/common/normalize.css';
 import '../styles/common/customize.css';
+import '~/styles/globals.scss'
 
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function _app({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+  
 }
-export default MyApp
+
+export default _app
