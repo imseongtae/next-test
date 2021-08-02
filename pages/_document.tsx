8a683f6f83c67d64c5a443b9eb57adec40f406c8
imseongtae <dev.haemil@gmail.com>
2021-08-03 03:01:09 +0900

    feat: Add _document

---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class _document extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>          
+          {/* <link rel="preconnect" href="https://fonts.gstatic.com" /> */}
+          {/* <link
+            href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="stylesheet"
+            href="//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css"
+          /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default _document;
